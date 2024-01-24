aeb254b827bbf9eccfd87174c1ab96a1ef075748
Adam Adhar <adamadhar28@gmail.com>
2024-01-24 16:05:24 +0100

    Add Form Repository

---
+++ FormCraft.Repositories.Contracts/IFormRepository.cs
@@ -0,0 +1,12 @@
+﻿using FormCraft.Entities;
+using FormCraft.Repositories.Contracts.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormCraft.Repositories.Contracts
+{
+    public interface IFormRepository : IReadRepository<Form>, IWriteRepository<Form> { }
+}
