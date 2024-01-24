8dff4454f547e5bf098baca3453d9bb96fa2244e
Alexandre <PC@DESKTOP-OC19THT>
2024-01-24 15:20:37 +0100

    Add QuestionRepository

---
+++ FormCraft.Repositories.Contracts/IQuestionRepository.cs
@@ -0,0 +1,16 @@
+﻿using FormCraft.Entities;
+using FormCraft.Repositories.Contracts.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormCraft.Repositories.Contracts
+{
+    public interface IQuestionRepository : IReadRepository<Question>, IWriteRepository<Question>
+    {
+
+
+    }
+}
