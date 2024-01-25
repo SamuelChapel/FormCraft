d83cadccab3fbb56a5ea2116765cc3a1c8826d11
Adam Adhar <adamadhar28@gmail.com>
2024-01-25 16:13:04 +0100

    FIx Confilcts

---
+++ FormCraft.Business.Contracts/Responses/Question/QuestionResponse.cs
@@ -0,0 +1,6 @@
+﻿using FormCraft.Business.Contracts.Common;
+
+namespace FormCraft.Business.Contracts.Responses.Question
+{
+    public record QuestionResponse(string Id, string Label, string FormId) : IRequest;
+}
