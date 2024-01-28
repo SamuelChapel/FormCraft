56704e94033be9be0d80ab0b369cd2ea908a9b0a
SamuelChapel <s.chapel@free.fr>
2024-01-28 17:03:32 +0100

    Add AnswerController

---
+++ FormCraft.Api/Controllers/AnswerController.cs
@@ -0,0 +1,61 @@
+﻿using FormCraft.Business.Contracts;
+using FormCraft.Business.Contracts.Requests.Answer;
+using FormCraft.Business.Contracts.Responses.Answer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormCraft.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AnswerController(IAnswerBusiness answerBusiness) : ControllerBase
+{
+    private readonly IAnswerBusiness _answerBusiness = answerBusiness;
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(404)]
+    public async Task<ActionResult<AnswerResponse>> GetById(string id)
+    {
+        var answer = await _answerBusiness.GetById(id);
+
+        return Ok(answer);
+    }
+
+    [HttpGet]
+    [ProducesResponseType(200)]
+    public async Task<ActionResult<List<AnswerResponse>>> GetAll()
+    {
+        var answers = await _answerBusiness.GetAll();
+
+        return Ok(answers);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(201)]
+    public async Task<ActionResult<AnswerResponse>> Create([FromBody] CreateAnswerRequest request)
+    {
+        var answer = await _answerBusiness.Create(request);
+
+        return CreatedAtAction(nameof(GetById), new { Id = answer.Id }, answer);
+    }
+
+    [HttpPut]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(404)]
+    public async Task<ActionResult<AnswerResponse>> Update(UpdateAnswerRequest request)
+    {
+        var answer = await _answerBusiness.Update(request);
+
+        return Ok(answer);
+    }
+
+    [HttpDelete]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> Delete(DeleteAnswerRequest id)
+    {
+        await _answerBusiness.Delete(id);
+
+        return NoContent();
+    }
+}
