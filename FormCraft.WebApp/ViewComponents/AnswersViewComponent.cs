356ed093bdfa08b54f714948dee645ccfde40088
SamuelChapel <s.chapel@free.fr>
2024-01-28 15:51:07 +0100

    Create Answer Views

---
+++ FormCraft.WebApp/ViewComponents/AnswersViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using FormCraft.Business.Contracts.Responses.Answer;
+using FormCraft.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormCraft.WebApp.ViewComponents;
+
+public class AnswersViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(QuestionTypeEnum questionTypeId, params AnswerResponse[] answers)
+    {
+        return questionTypeId switch
+        {
+            QuestionTypeEnum.Open => View("TextBoxAnswer", answers),
+            QuestionTypeEnum.RadioButton => View("RadioButtonAnswer", answers),
+            QuestionTypeEnum.Checkbox => View("CheckboxAnswer", answers),
+            QuestionTypeEnum.Dropdown => View("DropDownAnswer", answers),
+            _ => View(),
+        };
+
+    }
+}
