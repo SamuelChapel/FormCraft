8d5eae48e9b58315727db885a97baa5b4b9ca4ab
Adam Adhar <adamadhar28@gmail.com>
2024-02-05 12:24:43 +0100

    We did it !

--- FormCraft.WebApp/wwwroot/js/FormList.js
+++ FormCraft.WebApp/wwwroot/js/FormList.js
@@ -1,12 +1,72 @@
 ﻿
+const SearchForm = () => {
+    console.log('launcher de la mort qui tue')
+    let label = $('#labelInput').val();
+    let userId = "";
+    let order = $('#orderInput').val();
+
+    //let formTypeValues = $('input[name="IsFormTypePicked"]:checked').map(c => c.val()).get();
+    let formTypeValues = $('input[name="IsFormTypePicked"]:checked').map(function () {
+        return this.value;
+    }).get();
+
+    //let statusValues = $('input[name="IsStatusEnumPicked"]:checked').map(c => c.val()).get();
+    let statusValues = $('input[name="IsStatusEnumPicked"]:checked').map(function () {
+        return this.value;
+    }).get();
+
+
+    let request = {
+        Label: label,
+        CurrentUserId: userId,
+        IsStatusEnumPicked: statusValues,
+        IsFormTypePicked: formTypeValues,
+        Order: parseInt(order)
+    };
+
+    $.post("/Form/Search", request, data => {
+        $('#list-container').empty();
+        $('#list-container').html(data);
+    }
+    );
+}
+
+let timer;
+function addTimer() {
+
+    clearTimeout(timer);
+    timer = setTimeout(SearchForm, 300);
+    console.log('enter timer' + timer)
+}
+
+const onChangeEvents = () => {
+
+    let labelInput = document.querySelector('#labelInput');
+
+    labelInput.addEventListener('input', addTimer
+    );
+
+    //$("#labelInput").on("blur", e => {
+    //    clearTimeout(timer);
+    //    timer = setTimeout(() => SearchForm(), 0);
+    //    e.preventDefault();
+    //    console.log('Enter change event');
+    //});
+
+    document.querySelectorAll('.form-check-input')
+        .forEach(c => c.addEventListener('change', addTimer));
+}
+
+
 
 $(function () {
-    $(".delete").on('click', function (e) {
+    //Delete
+    $(".delete").on('click', e => {
         var formId = $(this).data("form-id");
 
         if (confirm('Are you sure ?')) {
             $.post("/Form/Delete", { id: formId },
-                function (data) {
+                data => {
                     alert('Deleted');
                     $("#row-" + formId).remove();
                 })
@@ -18,53 +78,8 @@ $(function () {
         e.preventDefault();
     });
 
-    $("#searchBtn").on('click', e => {
+    //Search
+    $("#searchBtn").on('click', e => SearchForm(e));
 
-        //let formList = $("#search-form-form");
-
-        let label = $('#labelInput').val();
-        let userId = "";
-        let order = $('#orderInput').val();
-
-        //let isTypeSurveyChecked = $('#formTypeInput-Survey').is(":checked");
-        //let isTypeCommentChecked = $('#formTypeInput-Comment').is(":checked");
-        //let isTypeEvaluationChecked = $('#formTypeInput-Evaluation').is(":checked");
-
-        //let isInProgressChecked = $('#statusInput-InProgress').is(":checked");
-        //let isValidatedChecked = $('#statusInput-Validated').is(":checked");
-        //let isClosedChecked = $('#statusInput-Closed').is(":checked");
-
-        let formTypeValues = $('input[name="IsFormTypePicked"]:checked').map(function () {
-            return $(this).val();
-        }).get();
-
-        let statusValues = $('input[name="IsStatusEnumPicked"]:checked').map(function () {
-            return $(this).val();
-        }).get();
-
-        let request = {
-            Label: label,
-            CurrentUserId: userId,
-            IsStatusEnumPicked: statusValues,
-            IsFormTypePicked: formTypeValues,
-            Order: parseInt(order)
-        };
-
-        console.log("request test", request);
-
-        $.post("/Form/Search", request, data => {
-
-            console.log("enter method");
-
-            $('#list-container').empty();
-            $('#list-container').html(data);
-        }
-        );
-
-        e.preventDefault();
-    });
+    onChangeEvents();
 });
-
-
-
-
